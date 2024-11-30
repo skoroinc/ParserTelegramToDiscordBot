a79fb87e689276a8a6a8820937aed643ae0def12
Rusichbtw <104261346+skoroinc@users.noreply.github.com>
2024-12-01 02:56:02 +0300

    Refact
    I rewrote the code according to the principles of OOP and SOLID. Implemented hiding tokens from bots.

---
+++ TelegramToDiscordBot/Models/TelegramToDiscordBot.Models.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramToDiscordBot.Models
+{
+    public class FileDetails
+    {
+        public required string FileId { get; set; }
+        public required string FileName { get; set; }
+        public required string FileType { get; set; }
+        public required Stream FileStream { get; set; }
+    }
+}
